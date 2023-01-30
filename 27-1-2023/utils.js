1ed03b4ff1a1498e31a3196ffa24e3adf5b7cfa4
Andrews46 <andrea4691ct@gmail.com>
2023-01-30 23:01:57 +0100

    esercizio

--- 27-1-2023/utils.js
+++ 27-1-2023/utils.js
@@ -16,6 +16,7 @@ const createCardGen = (data) => {
   cardEl.appendChild(imageEl);
   cardEl.addEventListener("click", () => {
     GET(data.id).then((selectedMovie) => {
+      // console.log(data.id);
       modalEl.appendChild(modalGen(selectedMovie));
       modalEl.style.display = "flex";
     });
@@ -24,7 +25,7 @@ const createCardGen = (data) => {
 };
 
 const modalGen = (data) => {
-  console.log(data);
+  // console.log(data);
   const modalEl = cE("div");
   const imgEl = cE("img");
   const wrapperTextEl = cE("div");
@@ -38,11 +39,13 @@ const modalGen = (data) => {
     "src",
     `https://image.tmdb.org/t/p/w500/${data.poster_path}`
   );
-  imgEl.setAttribute("alt", data.title);
-  titleEl.textContent = data.title;
+  imgEl.setAttribute("alt", data.name);
+  titleEl.className = "titleModal";
+  titleEl.textContent = data.name;
   overviewEl.textContent = data.overview;
-  releaseDateEl.textContent = data.release_date;
-  voteAverageEl.textContent = data.vote_average;
+  releaseDateEl.textContent = `Data uscita ${data.first_air_date}`;
+  // console.log(data.overview);
+  voteAverageEl.textContent = `Vote ${data.vote_average}`;
 
   wrapperTextEl.append(titleEl, overviewEl, releaseDateEl, voteAverageEl);
   modalEl.append(imgEl, wrapperTextEl);
