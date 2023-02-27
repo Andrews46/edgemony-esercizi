9a08154d1f8b9e5f1cb212ee2e787a9ea5f4a844
Andrea Cosentino <121038638+Andrews46@users.noreply.github.com>
2023-02-27 11:11:08 +0100

    Delete Code week directory

--- Code week/api_key.js
+++
@@ -1,5 +0,0 @@
-const API_KEY = "6bbfef46504b0bae66ef02f747a01517",
-  BASE_URL =
-    "https://api.openweathermap.org/data/2.5/weather?q=Palermo,&appid=6bbfef46504b0bae66ef02f747a01517";
-
-export { BASE_URL };
